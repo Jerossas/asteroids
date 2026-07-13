21474ae51fb891e4413e231b9daf4cf100182470
Jerossas <mosqueralozanojhoanferley@gmail.com>
2026-07-12 20:38:45 -0500

    feat: configure frames per second and fixed it to 60

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt: float = 0.0
+
     while True:
 
         log_state()
@@ -19,7 +22,9 @@ def main():
                 return
 
         screen.fill("black")
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
